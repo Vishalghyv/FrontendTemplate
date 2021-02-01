d34ef29801b2f890913adc37a147c040db2d6be3
Vishal <vishal38785@gmail.com>
2021-02-01 12:02:47 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./module.scss";
+
+function App() {
+  return (
+    <div className="progress-bar-v2">
+      <section className="header">
+        <div className="header-logo">Company Logo</div>
+        <div className="header-links">
+          <ul>
+            <li>About</li>
+            <li>Services</li>
+            <li>Help</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
